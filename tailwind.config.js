c87971b1c91c482f89c9ce48e1038626ee58b3fc
thomasschwinn <99867367+thomasschwinn@users.noreply.github.com>
2022-09-26 13:35:08 +0200

    added rosesblumen color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,26 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        trueGray: colors.trueGray,
-      },
-    },
-    fontFamily: {
-      sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      stock: [defaultTheme.fontFamily.sans],
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+	mode: "jit",
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				trueGray: colors.trueGray,
+				indigo: {
+					500: "#E5006C",
+					600: "#E5006C",
+				},
+			},
+		},
+		fontFamily: {
+			sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			stock: [defaultTheme.fontFamily.sans],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/aspect-ratio")],
 };
