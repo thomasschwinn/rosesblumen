93422fb8439fd3618beafaa2130f85b0afa84fa2
thomasschwinn <99867367+thomasschwinn@users.noreply.github.com>
2022-10-03 19:03:47 +0200

    added privacy policy

---
+++ pages/privacy-policy.js
@@ -0,0 +1,131 @@
+function datenschutz() {
+	return (
+		<div className="container mx-auto">
+			<h2>Datenschutz</h2>
+			Neonschwarz GmbH Datenschutzerklärung Der Schutz Ihrer persönlichen Daten
+			ist uns ein besonderes Anliegen. Wir verarbeiten Ihre Daten daher
+			ausschließlich auf Grundlage der gesetzlichen Bestimmungen (DSGVO, TKG
+			2003). In diesen Datenschutzinformationen informieren wir Sie über die
+			wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Website.
+			Cookies Unsere Website verwendet so genannte Cookies. Wir nutzen diese
+			dazu, unser Angebot nutzerfreundlich zu gestalten. Einige Cookies bleiben
+			auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Sie ermöglichen es
+			uns, Ihren Browser beim nächsten Besuch wiederzuerkennen. Cookies sind
+			kleine Textdateien, die vom Browser auf Ihrem Rechner gespeichert werden.
+			Dabei werden die Daten ausschließlich in anonymer oder pseudonymer Form
+			erhoben und gespeichert und erlauben keinen Rückschluss auf Ihre Person.
+			Wenn Sie dies nicht wünschen, so können Sie Ihren Browser so einrichten,
+			dass er Sie über das Setzen von Cookies informiert und Sie dies nur im
+			Einzelfall erlauben. Ohne Ihre ausdrückliche Zustimmung speichern unsere
+			Webserver keine Ihrer personenbezogenen Daten. Bei der Deaktivierung von
+			Cookies kann die Funktionalität unserer Website eingeschränkt sein.
+			Sollten Sie beim Aufruf unserer Webseite Ihre ausdrückliche Einwilligung
+			zur Speicherung und Nutzung bzw. Zum Einsatz von Cookies erteilt haben, so
+			verwenden unsere Webseiten an mehreren Stellen Cookies. Sie dienen dem
+			Zweck, unser Angebot nutzerfreundlicher und effektiver zu machen. Folgende
+			Cookies werden von uns gesetzt: Session-Cookies Google Analytics Die
+			Verwendung von Cookies ist weder gesetzlich noch vertraglich
+			vorgeschrieben und ist auch nicht für einen Vertragsabschluss
+			erforderlich. Es besteht daher keine Verpflichtung, Cookies zu verwenden
+			oder deren Verwendung zuzulassen. Die Rechtsgrundlage für die Verwendung
+			von Cookies ist nur die dazu von Ihnen erteilte Einwilligung (Art 6 Abs 1
+			lit a DSGVO). Wird die Einwilligung nicht erteilt, unterbleibt die
+			Verwendung von Cookies. Sie können die uns erteilte Einwilligung zum
+			Einsatz von Cookies jederzeit ohne Angabe von Gründen wiederrufen, Sie
+			können uns dazu unter den unten stehenden Kontaktdaten erreichen. Wir
+			weisen allerdings darauf hin, dass alle bis zum Widerruf vorgenommenen
+			Verarbeitungen/Übermittlungen weiterhin rechtmäßig bleiben. Web-Analyse
+			Unsere Website verwendet Funktionen des Webanalysedienstes Google
+			Analytics. Anbieter ist die Google Inc., 1600 Amphitheatre Parkway
+			Mountain View, CA 94043, USA. Dazu werden Cookies verwendet, die eine
+			Analyse der Benutzung der Website durch Ihre Benutzer ermöglicht. Die
+			dadurch erzeugten Informationen werden auf den Server des Anbieters
+			übertragen und dort gespeichert. Sie können dies verhindern, indem Sie
+			Ihren Browser so einrichten, dass keine Cookies gespeichert werden. Wir
+			haben mit dem Anbieter einen entsprechenden Vertrag zur
+			Auftragsdatenverarbeitung abgeschlossen. Ihre IP-Adresse wird erfasst,
+			aber umgehend (z.B. durch Löschung der letzten 8 Bit) pseudonymisiert.
+			Dadurch ist nur mehr eine grobe Lokalisierung möglich. Die
+			Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen des § 96
+			Abs 3 TKG sowie des Art 6 Abs 1 lit a (Einwilligung) und/oder f
+			(berechtigtes Interesse) der DSGVO. Unser Anliegen im Sinne der DSGVO
+			(berechtigtes Interesse) ist die Verbesserung unseres Angebotes und
+			unseres Webauftritts. Da uns die Privatsphäre unserer Nutzer wichtig ist,
+			werden die Nutzerdaten pseudonymisiert. Die Nutzerdaten werden für die
+			Dauer von 26 Monaten aufbewahrt. Einbindung sonstiger Dienste und Inhalte
+			Dritter Auf einigen Seiten dieses Onlineangebotes werden Inhalte
+			eingebunden. Die Nutzung von Onlineangeboten Dritter setzt immer voraus,
+			dass die Anbieter dieser Inhalte die IP-Adresse der Nutzer wahrnehmen.
+			Denn ohne eine IP-Adresse, könnten die Inhalte nicht an den Browser des
+			jeweiligen Nutzers versendet werden. Die IP-Adresse ist damit für die
+			Darstellung dieser Inhalte erforderlich. Wir bemühen uns nur solche
+			Inhalte zu verwenden, deren jeweilige Anbieter die IPAdresse lediglich zur
+			Auslieferung der Inhalte verwenden. Jedoch haben wir keinen Einfluss
+			darauf, falls ein Drittanbieter die IP-Adresse zum Beispiel für
+			statistische Zwecke speichert. Soweit uns dieses Verhalten bekannt ist,
+			klären wir die Nutzer darüber auf. Google Maps Auf unserer Webseite nutzen
+			wir das Angebot von Google Maps. Dadurch können wir Ihnen interaktive
+			Karten direkt in der Webseite anzeigen und ermöglichen Ihnen die
+			komfortable Nutzung der Karten-Funktion. Durch den Besuch auf der Webseite
+			erhält Google die Information, dass Sie die entsprechende Unterseite
+			unserer Webseite aufgerufen haben. Dies erfolgt unabhängig davon, ob
+			Google ein Nutzerkonto bereitstellt, über das Sie eingeloggt sind, oder ob
+			kein Nutzerkonto besteht. Wenn Sie bei Google eingeloggt sind, werden Ihre
+			Daten direkt Ihrem Konto zugeordnet. Wenn Sie die Zuordnung mit Ihrem
+			Profil bei Google nicht wünschen, müssen Sie sich vor Aktivierung des
+			Buttons ausloggen. Google speichert Ihre Daten als Nutzungsprofile und
+			nutzt sie für Zwecke der Werbung, Marktforschung und/oder bedarfsgerechten
+			Gestaltung seiner Webseite. Ihnen steht ein Widerspruchsrecht zu gegen die
+			Bildung dieser Nutzerprofile, wobei Sie sich zur Ausübung dessen an Google
+			richten müssen. Weitere Informationen zu Zweck und Umfang der
+			Datenerhebung und ihrer Verarbeitung durch Google erhalten Sie neben
+			weiteren Informationen zu Ihren diesbezüglichen Rechten und
+			Einstellungsmöglichkeiten zum Schutze Ihrer Privatsphäre unter:
+			https://www.google.de/intl/de/policies/privacy. Verarbeitung
+			personenbezogener Daten bei Kontaktaufnahme über die Webseiten Zu den
+			personenbezogenen Daten zählen: Ihre Personalien: z.B. Name, Adresse,
+			Emailadresse, Telefonnummer, Geburtsdatum und Geschlecht; Daten über Ihre
+			Zahlungsmittel: z.B. Kreditkartenunternehmen, Kreditkartennummern, Name
+			des Kreditkarteninhabers, Gültigkeitsdauer und Prüfziffer, Daten über Ihre
+			Einkäufe bei Rose Mwende Schwinn: Kassenbonnummer, Rechnungsnummer,
+			Kaufdatum- und uhrzeit, Artikel, Stückzahl und Preis. Personenbezogene
+			Daten werden auf unserer Website verarbeitet, wenn Sie persönliche Angaben
+			auf einem der angebotenen Kontaktformulare eingeben. Diese Daten, sowie
+			der Inhalt Ihrer Kontaktaufnahme, werden an den zuständigen
+			Ansprechpartner bei Rose Mwende Schwinn weitergeleitet, der Ihre Daten
+			ausschließlich zur Bearbeitung Ihres Anliegens verwendet. Eine darüber
+			hinausgehende Verarbeitung erfolgt nicht. Widerspruch gegen Datenerfassung
+			Sie können die Erfassung Ihrer Daten durch Google Analytics verhindern,
+			indem Sie ein Opt-Out-Cookie setzen, der die Erfassung Ihrer Daten bei
+			zukünftigen Besuchen dieser Website verhindert. Mehr Informationen zum
+			Umgang mit Nutzerdaten bei Google Analytics finden Sie in der
+			Datenschutzerklärung von
+			Google:https://support.google.com/analytics/answer/6004245?hl=de
+			SSL-Verschlüsselung Diese Seite nutzt aus Gründen der Sicherheit und zum
+			Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel der
+			Anfragen, die Sie an uns als Seitenbetreiber senden, eine
+			SSL-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran,
+			dass die Adresszeile des Browsers von „http://“ auf „https://“ wechselt
+			und an dem Schloss-Symbol in Ihrer Browserzeile. Wenn die SSL
+			Verschlüsselung aktiviert ist, können die Daten, die Sie an uns
+			übermitteln, nicht von Dritten mitgelesen werden. Recht auf Auskunft,
+			Löschung, Sperrung Sie haben jederzeit das Recht auf unentgeltliche
+			Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft
+			und Empfänger und den Zweck der Datenverarbeitung sowie ein Recht auf
+			Berichtigung, Sperrung oder Löschung dieser Daten. Hierzu sowie zu
+			weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit
+			unter der im Impressum und unterhalb angegebenen Adresse an uns wenden.
+			Ihre Rechte Ihnen stehen grundsätzlich die Rechte auf Auskunft,
+			Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit, Widerruf und
+			Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen
+			das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche
+			sonst in einer Weise verletzt worden sind, können Sie sich bei der
+			Aufsichtsbehörde beschweren. In Österreich ist dies die
+			Datenschutzbehörde: https://www.dsb.gv.at/ Sie erreichen uns unter
+			folgenden Kontaktdaten: Rose Mwende Schwinn, Franz-Hochedlinger-Gasse 9/8,
+			1020 Wien Handelsgericht Wien Unternehmensgegenstand: Handel
+		</div>
+	);
+}
+
+export default datenschutz;
