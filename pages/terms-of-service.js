802e4a61b0be95fc21cac6a9e685a3b02705ca75
thomasschwinn <99867367+thomasschwinn@users.noreply.github.com>
2022-10-03 19:10:33 +0200

    added terms of service

---
+++ pages/terms-of-service.js
@@ -0,0 +1,208 @@
+function agb() {
+	return (
+		<div className="container mx-auto">
+			1. Allgemeines 1.1. Allgemeine Geschäftsbedingungen (AGB) für Kunden Rose
+			Mwende Schwinn, Franz-Hochedlinger-Gasse 9/8, 1020 Wien. 1.2. Unser Waren-
+			und Dienstleistungsangebot richtet sich ausschließlich an unbeschränkt
+			geschäftsfähige Personen sowie juristische Personen. Nicht oder beschränkt
+			geschäftsfähige Personen sind nur unter Mitwirkung und Zustimmung ihres
+			gesetzlichen Vertreters zur Inanspruchnahme des angebotenen Sortiments
+			befugt. Abbildungen auf unserer Homepage dienen lediglich der
+			Produktpräsentation und stellen kein rechtsverbindliches Angebot dar. 2.
+			Vertragsabschluss, Aufwandsentschädigung 2.1. Für den Abschluss eines
+			Vertrages zwischen einem Kunden und unserem Unternehmen bedarf es einer
+			schriftlichen Unterfertigung der Auftragsbestätigung durch den Kunden samt
+			Übermittlung an uns sowie Einlagen der vereinbarten Anzahlung. Ein bloßes
+			Unterfertigen unserer Auftragsbestätigung ist für einen Vertragsabschluss
+			nicht ausreichend. 2.2. Sofern ein Kunde bloß die unterzeichnete
+			Auftragsbestätigung an uns übermittelt, ohne die vereinbarte Anzahlung zu
+			leisten, verrechnen wir eine Aufwandsentschädigung von 10% des
+			Auftragsvolumens. Nachträgliche Änderungswünsche, insbesondere im Hinblick
+			auf bereits in Arbeit befindliche Möbel und Raumausstattungswaren können
+			nicht akzeptiert werden. 3. Warenpreise, akzeptierte Zahlungsarten,
+			Versandkosten Sämtliche Preise verstehen sich inklusive gesetzlicher
+			Umsatzsteuer und zuzüglich etwaiger Versandkosten. Wir bieten als
+			Zahlungsmöglichkeiten Vorauskasse, Sofortüberweisung, Kreditkarte,
+			……………………………… an. Wir behalten uns den Ausschluss bestimmter Zahlungsarten
+			im Einzelfall vor. 4. Zahlung Die Zahlung erfolgt wie in der
+			Auftragsbestätigung schriftlich festgehalten und ist bei
+			Lieferbereitschaft beziehungsweise Übernahme der Ware fällig. Spätestens
+			bei der Warenübernahme durch den Kunden ist der vereinbarte
+			Rechnungsbetrag vollständig zu begleichen, andernfalls die Ware nicht
+			ausgefolgt wird. Kommt der Besteller seiner Zahlungsverpflichtung trotz
+			Mahnung und Setzung einer 14-tägigen Frist nicht nach, so sind wir
+			berechtigt, vom Kaufvertrag zurückzutreten. Im Falle des Rücktritts vom
+			Kaufvertrag sind wir berechtigt, die geleistete Anzahlung als
+			Konventionalstrafe einzubehalten. Weder in den Lieferkosten noch im Preis
+			des Produktes ist die Montage der angelieferten Ware enthalten. 5.
+			Eigentumsvorbehalt Die Waren bleiben bis zu ihrer vollständigen Bezahlung
+			unser Eigentum. 6. Lieferung, Liefertermine, Lieferfrist, Abnahmeverzug,
+			Vertragsrücktritt 6.1. Die Lieferfrist gilt erst ab Erhalt der Anzahlung
+			von mindestens 50 Prozent des Auftragswertes. Ein Rücktritt vom
+			Kaufvertrag aufgrund verspäteter Lieferung ist nur dann möglich, wenn uns
+			eine Nachfrist von 8 Wochen eingeräumt wird. Können wir unsere Lieferfrist
+			nur teilweise erfüllen, so berechtigt dies nicht zum Rücktritt vom
+			gesamten Vertrag. Sollten bloß einzelne Bestandteile nicht fristgerecht
+			geliefert werden, wird die Rechnungsforderung für die gelieferten Teile
+			fällig. Schadenersatzansprüche wegen verspäteter Lieferung sind
+			ausgeschlossen. Der vollständige Rechnungsbetrag ist bei Übernahme
+			beziehungsweise Lieferbereitschaft fällig. 6.2. Unsere Verkaufspreise
+			beinhalten keine Kosten für Zustellung, Montage oder Aufstellung. Auf
+			Wunsch werden diese Leistungen gegen gesonderte Zahlung von uns erbracht
+			beziehungsweise organisiert. Dabei werden für Transport beziehungsweise
+			Zustellung die tatsächlich aufgewendeten Kosten samt einem angemessenen
+			Regiekostenaufschlag in Rechnung gestellt. 6.3. Hat der Kunde die Ware
+			nicht wie vereinbart übernommen (Annahmeverzug) sind wir berechtigt, die
+			Ware einzulagern, wobei der Kunde eine Lagergebühr von 0,1% des
+			Bruttorechnungsbetrages bzw. mindestens EUR 50,00 jeweils pro angefangenen
+			Kalendertag zu bezahlen hat. Gleichzeitig sind wir berechtigt, entweder
+			auf Vertragserfüllung zu bestehen, oder nach Setzung einer angemessenen,
+			mindestens zwei Wochen umfassenden Nachfrist, vom Vertrag zurückzutreten
+			und die Ware anderwärtig zu verwerten. Die Kosten einer neuerlichen
+			Lieferung sind vom Kunden zu bezahlen. 6.4. Ist der Kunde Verbraucher im
+			Sinne des KSchG, geht die Gefahr des Untergangs und der zufälligen
+			Verschlechterung der Ware mit Übergang der Ware an den Kunden oder an
+			dessen Vertreter oder an die Person, die an der Lieferadresse anwesend ist
+			und der die Ware übergeben werden kann, über. Wenn der Kunde mit der
+			Übernahme der Ware in Verzug ist, geht die Gefahr des Untergangs und der
+			zufälligen Verschlechterung der Ware mit Annahmeverzug auf den Kunden
+			über. 6.5. Als Verzugszinsen werden 5 Prozent per annum zuzüglich
+			Umsatzsteuer verrechnet. 6.6. Sollten bestellte Waren aus Gründen, die
+			außerhalb unseres Einflussbereiches liegen, nicht mehr lieferbar sein,
+			werden wir Sie unverzüglich benachrichtigen. Schadenersatzansprüche sind
+			ausgeschlossen. 7. Abmessung Der Kunde ist verpflichtet zu überprüfen, ob
+			die vom Kunden bestellten Waren an den vom Kunden ausgewählten
+			Bestimmungsort aufgrund der individuellen Abmessungen und Beschaffenheit
+			transportiert und aufgestellt werden können. Wir übernehmen keine Haftung
+			und Gewährleistung für die Unmöglichkeit des Transports. Die Gefahr des
+			Transports aufgrund der Abmessung der Ware und aufgrund der individuellen
+			Abmessungen trägt der Kunde. 8. Gewährleistung/Folgeschäden 8.1. Der
+			Kunde, der Konsument im Sinne des Konsumentenschutzgesetzes ist, hat die
+			Wahl, ob er die Verbesserung oder den Austausch der Ware will. Wir sind
+			jedoch berechtigt, die vom Kunden gewählte Abhilfe zu verweigern, wenn
+			diese entweder unmöglich oder im Verhältnis zur anderen Abhilfe nur mit
+			unverhältnismäßigem Aufwand möglich ist. Ist eine Verbesserung nicht
+			möglich, kann der Kunde nach seiner Wahl entweder Preisminderung oder,
+			sofern es sich nicht um einen geringfügigen Mangel handelt, Wandlung des
+			Vertrages verlangen. 8.2. Ist der Kunde Unternehmer, haben wir das
+			Wahlrecht, ob wir für Mängel die Verbesserung oder den Austausch der Ware
+			gewähren. Der Kunde, der Unternehmer ist, hat Mängel der Ware, die er bei
+			ordnungsgemäßem Geschäftsgang nach Ablieferung durch Untersuchen
+			festgestellt hat oder feststellen hätte müssen, binnen einer Frist von 7
+			Tagen ab Empfang der Ware schriftlich anzuzeigen, anderenfalls sind
+			Gewährleistungsansprüche ausgeschlossen. Zur Fristwahrung genügt die
+			rechtzeitige Absendung der Mängelrüge. Den Unternehmenskunden trifft die
+			Beweislast für sämtliche Anspruchsvoraussetzungen, insbesondere für
+			Vorliegen des Mangels, Zeitpunkt der Feststellung des Mangels und
+			Rechtzeitigkeit der Mängelrüge. 8.3. Ist der Kunde Unternehmer, gilt als
+			Beschaffenheit der Ware grundsätzlich nur die Produktbeschreibung des
+			Herstellers als vereinbart. Öffentliche Äußerungen, Anpreisungen oder
+			Werbung des Herstellers stellen keine Beschaffenheitsangabe der Ware dar.
+			8.4. Soweit Einrichtungsgegenstände aus Holz gefertigt werden, ist zu
+			berücksichtigen, dass natürliche Merkmale wie Astlöcher, Risse oder
+			unterschiedliche Farbschattierungen im geringfügigen Maß den Wert der
+			Einrichtungsgegenstände nicht mindern. Auch handelsübliche, geringfügige
+			Abweichungen bei Farben oder Mustern gelten als akzeptiert. 9. Mahn- und
+			Inkassospesen Der Vertragspartner (Kunde) verpflichtet sich für den Fall
+			des Verzuges, die dem Gläubiger entstehenden Mahn- und Inkassospesen,
+			soweit sie zur zweckentsprechenden Rechtsverfolgung notwendig sind, zu
+			ersetzen, wobei er sich im speziellen verpflichtet, maximal die
+			Vergütungen des eingeschalteten Inkassoinstitutes zu ersetzen, die sich
+			aus der Verordnung des BMwA über die Höchstsätze von Inkassoinstituten
+			gebührenden Vergütungen ergeben. Sofern der Gläubiger das Mahnwesen selbst
+			betreibt, verpflichtet sich der Schuldner pro erfolgter Mahnung einen
+			Betrag von EUR 12,00 sowie für die Evidenzhaltung des Schuldverhältnisses
+			ihm Mahnwahnwesen pro Halbjahr einen Betrag von EUR 5,00 zu bezahlen. 10.
+			Datenschutz, Adressänderung, Urheberrecht Der Kunde erteilt seine
+			Zustimmung, dass auch die im Kaufvertrag mitenthaltenen personenbezogenen
+			Daten in Erfüllung dieses Vertrages von uns automationsunterstützt
+			gespeichert und verarbeitet werden. Der Kunde ist verpflichtet, uns
+			Änderungen seiner Wohn- beziehungsweise Geschäftsadresse bekanntzugeben,
+			solange das vertragsgegenständliche Rechtsgeschäft nicht beiderseitig
+			vollständig erfüllt ist. Wird diese Mitteilung unterlassen, so gelten
+			Erklärungen auch dann als zugegangen, falls sie an die zuletzt
+			bekanntgegebene Adresse gesendet werden. Pläne, Skizzen oder sonstige
+			technische Unterlagen bleiben ebenso wie Muster, Kataloge, Prospekte,
+			Abbildungen und dergleichen stets unser geistiges Eigentum. Der Kunde
+			erhält daran keine wie immer gearteten Wertnutzungs- oder
+			Verwertungsrechte. Einzelheiten entnehmen Sie bitte unserer
+			Datenschutzerklärung. 11. Widerrufsrecht / Rücktrittserklärung für
+			Kaufverträge im Online Handel und Fernabsatzgeschäft für Verbraucher iSd
+			KSchG 11.1. Wiederrufsrecht / Rücktrittserklärung: Der Kunde hat das
+			Recht, binnen 14 Tagen ohne Angabe von Gründen diesen Vertrag zu
+			widerrufen. Die Widerrufsfrist beträgt 14 Tage ab dem Tag, a) an dem ein
+			Kunde oder ein von ihm benannter Dritter, der nicht der Beförderer ist,
+			die Waren in Besitz genommen hat, wenn der Kunde eine Ware oder mehrere
+			Waren im Rahmen einer einheitlichen Bestellung bestellt hat und die Waren
+			bzw. die Waren einheitlich geliefert wird bzw. werden; oder b) an dem der
+			Kunde oder ein von ihm benannter Dritter, der nicht der Beförderer ist,
+			die letzte Ware in Besitz genommen hat, wenn der Kunde mehrere Waren im
+			Rahmen einer einheitlichen Bestellung bestellt hat und die Waren getrennt
+			geliefert werden; oder c) an dem der Kunde oder ein von ihm benannter
+			Dritter, der nicht der Beförderer ist, die letzte Teilsendung oder das
+			letzte Stück in Besitz genommen hat, wenn der Kunde eine Ware bestellt
+			hat, die in mehreren Teilsendungen oder Stücken geliefert wird. Um das
+			Widerrufsrecht auszuüben, muss der Kunde mich Rose Mwende Schwinn,
+			Franz-Hochedlinger-Gasse 9/8, 1020 Wien mittels einer eindeutigen
+			Erklärung beispielsweise postalisches Schreiben oder Email über den
+			Entschluss, diesen Vertrag zu widerrufen, informieren. Der Kunde kann
+			dafür das Muster – Widerrufsformular verwenden, das jedoch nicht
+			vorgeschrieben ist. Das Musterformular kann hier (Musterwiderrufsformular)
+			abgerufen werden. Zur Wahrung der Widerrufsfrist reicht es aus, wenn der
+			Kunde die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der
+			Widerrufsfrist absendet. 11.2. Folgen des Widerrufs: Wenn der Kunde den
+			Vertrag widerruft, hat Rose Mwende Schwinnn alle Zahlungen, die Rose
+			Mwende Schwinn vom Kunden erhalten hat, einschließlich der Lieferkosten
+			(mit Ausnahme der zusätzlichen Kosten, die sich daraus ergeben, dass der
+			Kunde eine andere Art der Lieferung als die von Rose Mwende Schwinn
+			angebotene, günstigste Standardlieferung gewählt hat), unverzüglich und
+			spätestens binnen 14 Tagen ab dem Tag zurückzuzahlen, an dem die
+			Mitteilung über den Widerruf dieses Vertrages bei Rose Mwende Schwinn
+			eingegangen ist. Für diese Rückzahlung verwendet Rose Mwende Schwinn
+			dasselbe Zahlungsmittel, das der Kunde bei der ursprünglichen Transaktion
+			eingesetzt hat. Bei Wiederruf von einzelnen Artikeln der Bestellung werden
+			anteilsmäßig die Lieferkosten des entsprechenden Warenwertes
+			zurückerstattet. Rose Mwende Schwinn kann die Rückzahlung verweigern, bis
+			Rose Mwende Schwinn die Waren wieder zurückerhalten hat oder bis der Kunde
+			den Nachweis erbracht hat, dass er die Waren zurückgesandt hat, je
+			nachdem, welches der frühere Zeitpunkt ist. Der Kunde muss die Waren
+			unverzüglich und in jedem Fall spätestens binnen 14 Tagen nach Absendung
+			der Rücktrittserklärung retournieren. Die Frist ist gewahrt, wenn der
+			Kunde die Waren vor Ablauf der Frist von 14 Tagen absendet. Der Kunde
+			trägt die unmittelbaren Kosten der Rücksendung paketversandfähiger Waren.
+			Der Kunde trägt auch die unmittelbaren Kosten der Rücksendung
+			nichtpaketversandfähiger Waren (z.B. über Spedition). Der Kunde muss für
+			einen etwaigen Wertverlust der Waren nur aufkommen, wenn dieser
+			Wertverlust auf einen zur Prüfung der Beschaffenheit, Eigenschaften oder
+			Funktionsweise der Waren nicht notwendigen Umgang von Kunden
+			zurückzuführen ist. 11.3. Ausschluss des Widerrufsrechtes Das
+			Widerrufsrecht besteht nicht bei Verträgen zur Lieferung von Waren, die
+			nicht vorgefertigt sind und für deren Herstellung eine individuelle
+			Auswahl oder Bestimmung durch den Verbraucher maßgeblich ist oder die
+			eindeutig auf die persönlichen Bedürfnisse des Verbrauchers zugeschnitten
+			sind. 12. Schlussbestimmungen 12.1. Für sämtliche Vertragsverhältnisse
+			wird die Anwendung des österreichischen Rechts vereinbart, die Anwendung
+			des UN-Kaufrechts wird ausgeschlossen. 12.2. Für alle unmittelbar oder
+			mittelbar aus diesem Vertragsverhältnis entstehenden Streitigkeiten wird
+			die Zuständigkeit des örtlich für unseren Firmensitz zuständigen Gerichtes
+			vereinbart. Verbraucher im Sinne des Konsumentenschutzgesetzes können auch
+			an ihrem Wohnsitz klagen. Wir haben das Recht den Kunden auch an seinem
+			Sitz oder Wohnsitz zu klagen. 12.3. Sollten einzelnen Bestimmungen des
+			Vertrages mit Kunden einschließlich dieser Allgemeinen
+			Geschäftsbedingungen ganz oder teilweise unwirksam sein oder werden, so
+			wird hierdurch die Gültigkeit der übrigen Bestimmungen nicht berührt. Die
+			ganz oder teilweise unwirksame Regelung wird durch eine Regelung ersetzt,
+			die dem wirtschaftlichen Zweck der unwirksamen möglichst nahe kommt. 12.4.
+			Diese Allgemeinen Geschäftsbedingungen gelten für alle Angebote, Verträge,
+			Lieferung oder sonstigen Leistungen durch uns. Abänderungen oder
+			Nebenabreden bedürfen zu ihrer Gültigkeit der schriftlichen Bestätigung
+			durch im Firmenbuch eingetragene vertretungsbefugte Personen unserer
+			Gesellschaft und gelten nur für den einzelnen Geschäftsfall. Alle anderen
+			Mitarbeiter/innen sind nicht bevollmächtigt, Änderungen oder Nebenabreden
+			zu diesen Allgemeinen Geschäftsbedingungen zu vereinbaren. Abweichenden,
+			entgegenstehenden oder auch bloß zusätzlichen beziehungsweise ergänzenden
+			Vertragsbedingungen des Kunden wird ausdrücklich widersprochen. Rose
+			Mwende Schwinn
+		</div>
+	);
+}
